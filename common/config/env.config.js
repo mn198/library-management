03575694f264fcec281a49a682fc6983e9a1484e
unknown <minhnhut1329@gmail.com>
2019-11-20 14:00:53 +0700

    last time

--- common/config/env.config.js
+++ common/config/env.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     "db_url": "mongodb://mn198:mmn198@ds135796.mlab.com:35796/mn-library",
-    "port": 3600,
-    "appEndpoint": "http://localhost:3600",
-    "apiEndpoint": "http://localhost:3600",
+    "port": 8080,
+    "appEndpoint": "http://localhost:8080",
+    "apiEndpoint": "http://localhost:8080",
     "jwt_secret": "myS33!!creeeT",
     "jwt_expiration_in_seconds": 36000,
     "environment": "dev",
