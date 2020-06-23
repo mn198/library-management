8343139fd2d42a170e71641d8189f842917ec4e7
minhnhut1329@gmail.com <minhnhut1329@gmail.com>
2020-06-23 21:40:58 +0700

    ...

--- app.js
+++ app.js
@@ -45,11 +45,11 @@ LendingRouter.routesConfig(app);
 
 
 /*Adds the react production build to serve react requests*/
-// app.use('/', express.static('grand_new_client/build'));
+app.use('/', express.static('client/build'));
 /*React root*/
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'grand_new_client', 'build', 'index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+});
 
 const server = app.listen(process.env.PORT || config.port, () => {
     console.log('app listening at port %s', config.port);
